9e6116fe56b2e8fafd3845490798b2e3ca898c22
Andrew Mehta <andmehta@icloud.com>
2020-03-03 14:43:14 -0600

    added testing code to var_dump stuff still no movement on

--- login_page.php
+++ login_page.php
@@ -16,6 +16,10 @@
 
   <!-- Put your PHP to log someone in here... Includes forwarding, storing sessions, etc. -->
   <?php
+    require 'login.php';
+    $conn = new mysqli($hn, $un, $pw, $db);
+    if($conn->connect_error)
+      die($conn->connect_error);
 
 
   ?>
@@ -31,10 +35,60 @@
     <label>Username: </label>
     <input type="text" name="username"> <br>
     <label>Password: </label>
-    <input type="password" name="password"> <br>
+    <!--TODO temporarily plaintext change type back to password -->
+    <input type="text" name="password"> <br>
     <input type="submit" value="Log in">
   </form>
 
+  <?php
+    //santize input strings to prevent funny business
+    function sanitizeString($var) {
+      $var = stripslashes($var);
+      $var = strip_tags($var);
+      $var = htmlentities($var);
+      return $var;
+    }
+
+    // TESTING
+    var_dump($_POST);
+
+    //get the username in a usable state
+    $username = sanitizeString($_POST['username']);
+
+    //Hide the password using hashing and salt
+    $salt1    = "qm&h*";
+    $salt2    = "pg!@";
+    $password = sanitizeString($_POST['password']);
+    $token    = hash('ripemd128', "$salt1$password$salt2");
+
+    //query the database for the matching username and password
+    $query = "SELECT username
+              FROM lab5_users
+              WHERE EXISTS username = $username
+              AND password = $token";
+
+    //TESTING
+    echo '<br>---------------------------------------<br>';
+    var_dump($username); //should be bsmith
+    var_dump($token); //should be 32aa0c466818e1ccba25b8793db98c94
+    echo '<br>---------------------------------------<br>';
+    var_dump($query);
+
+    $result = $conn->query($query);
+    //TESTING
+    var_dump($result);
+
+    //$result = "blah";
+    //echo mysqli_num_rows($result);
+    //if the result returns true, then this user exists within the db
+    if($result && isset($_POST['username'])) {
+      //TODO navigate to either User or Admin page depeding on result
+      echo "<p>Success $result</p>";
+    }
+    else if(isset($_POST['username'])){
+      echo "<p>Fail</p>";
+    }
+   ?>
   <p style="font-style:italic">
     Placeholder for "forgot password" link<br><br>
     Placeholder for "create account" link
