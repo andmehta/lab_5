e941022eac7ab2861e2ef0c909317f7455506479
Andrew Mehta <38476199+andmehta@users.noreply.github.com>
2020-03-05 13:29:22 -0600

    Wrong access error checking implemented
    * added check that prevents someone who is either NOT logged in, or NOT an admin from viewing the page, and displayed a unique error message for either scenario
    
    * added check that prevents someone who is either NOT logged in, or NOT a user from viewing the page, and displayed a unique error message for either scenario. Also SHREK
    
    * Update README.md
    
    * fixed alt text on shrek image

--- admin_page.php
+++ admin_page.php
@@ -24,53 +24,69 @@
     //get the sessions from the login page
     session_start();
 
-    //greet the user personally
-    $username = $_SESSION['currentUser'];
-    echo "<p>Welcome back $username</p>
-          <p>Here's all the orders in the database...</p>";
+    // if the user has not logged in first, display this page and say they need to login_page
+    // please don't dock points for adding picture I'm just trying to have fun :)
+    if(!isset($_SESSION['currentUser']) && !isset($_SESSION['type']) || $_SESSION['type'] != 'admin') {
+      echo "<h1>What are you DOING in my PAGE??!?</h1>";
+      echo "<img src=\"Shrek_(character).png\" alt=\"SHREK\" width=\"190\" height=\"390\">";
 
-    echo "<table>
-    <tr>
-      <th>Order ID</th>
-      <th>Username</th>
-      <th>Order Total</th>
-      <th>Order Quantity</th>
-      <th>Shipping Method</th>
-    </tr>";
+      $error = $_SESSION['type'] != 'admin' && isset($_SESSION['type']) ? "You cannot be here as you are not an admin." : "You are not currently logged in at all";
+      echo "<p>$error</p>";
+      // destroy any session keys to force return to login page
+      session_start();
 
-    //login to mySQL instance in order to query
-    require 'login.php';
-    $conn = new mysqli($hn, $un, $pw, $db);
-    if($conn->connect_error)
-      die($conn->connect_error);
+      session_unset();
+      session_destroy();
+      echo "<p><a href=\"login_page.php\">Log in</a></p>";
+    } else {
+      //greet the user personally
+      $username = $_SESSION['currentUser'];
+      echo "<p>Welcome back $username</p>
+            <p>Here's all the orders in the database...</p>";
 
-    // query the database for ALL the orders
-    $query = "SELECT * FROM lab5_orders";
-    $result = $conn->query($query);
+      echo "<table>
+      <tr>
+        <th>Order ID</th>
+        <th>Username</th>
+        <th>Order Total</th>
+        <th>Order Quantity</th>
+        <th>Shipping Method</th>
+      </tr>";
 
-    // display each order as well as who ordered it
-    while($row = $result->fetch_array() ){
-    	$orderID = $row['orderID'];
-      $orderUsername = $row['username'];
-    	$orderTotal = $row['orderTotal'];
-    	$quantity = $row['quantity'];
-    	$shipping = $row['shipping'];
+      //login to mySQL instance in order to query
+      require 'login.php';
+      $conn = new mysqli($hn, $un, $pw, $db);
+      if($conn->connect_error)
+        die($conn->connect_error);
 
-    	echo "
-    		<tr>
-    			<td>$orderID</td>
-          <td>$orderUsername</td>
-    			<td style =\"text-align: center\">$orderTotal</td>
-    			<td>$quantity</td>
-    			<td>$shipping</td>
-    		</tr>
-        ";
+      // query the database for ALL the orders
+      $query = "SELECT * FROM lab5_orders";
+      $result = $conn->query($query);
+
+      // display each order as well as who ordered it
+      while($row = $result->fetch_array() ){
+      	$orderID = $row['orderID'];
+        $orderUsername = $row['username'];
+      	$orderTotal = $row['orderTotal'];
+      	$quantity = $row['quantity'];
+      	$shipping = $row['shipping'];
+
+      	echo "
+      		<tr>
+      			<td>$orderID</td>
+            <td>$orderUsername</td>
+      			<td style =\"text-align: center\">$orderTotal</td>
+      			<td>$quantity</td>
+      			<td>$shipping</td>
+      		</tr>
+          ";
+      }
+
+      echo "</table>
+      <p><a href=\"logout_page.php\">Log out</a></p>";
     }
-
   ?>
-</table>
 
-<p><a href="logout_page.php">Log out</a></p>
 </body>
 
 </html>
--- user_page.php
+++ user_page.php
@@ -24,46 +24,62 @@
     // get session from login page
     session_start();
 
-    // greet user personally
-    $username = $_SESSION['currentUser'];
-    echo "<p>Welcome back $username</p>
-          <p>Here's your past orders...</p>";
+    // if the user has not logged in first, display this page and say they need to login_page
+    // please don't dock points for adding picture I'm just trying to have fun :)
+    if(!isset($_SESSION['currentUser']) && !isset($_SESSION['type']) || $_SESSION['type'] != 'user') {
+      echo "<h1>What are you DOING in my PAGE??!?</h1>";
+      echo "<img src=\"Shrek_(character).png\" alt=\"SHREK\" width=\"190\" height=\"390\">";
 
-    echo "<table>
-    <tr>
-      <th>Order ID</th>
-      <th>Order Total</th>
-      <th>Order Quantity</th>
-      <th>Shipping Method</th>
-    </tr>";
+      $error = $_SESSION['type'] != 'user' && isset($_SESSION['type']) ? "You cannot be here as you are not a user." : "You are not currently logged in at all";
+      echo "<p>$error</p>";
+      // destroy session keys to force a return to the login screen
+      session_start();
 
-    // login to mySQL instance in order to query
-    require 'login.php';
-    $conn = new mysqli($hn, $un, $pw, $db);
-    if($conn->connect_error)
-      die($conn->connect_error);
+      session_unset();
+      session_destroy();
+      echo "<p><a href=\"login_page.php\">Log in</a></p>";
+    } else {
+      // greet user personally
+      $username = $_SESSION['currentUser'];
+      echo "<p>Welcome back $username</p>
+            <p>Here's your past orders...</p>";
 
-    // query the database for all orders made by the user
-    $query = "SELECT * FROM lab5_orders WHERE username = '$username'";
-    $result = $conn->query($query);
-    while($row = $result->fetch_array()){
-    	$orderID = $row['orderID'];
-    	$orderTotal = $row['orderTotal'];
-    	$quantity = $row['quantity'];
-    	$shipping = $row['shipping'];
+      echo "<table>
+      <tr>
+        <th>Order ID</th>
+        <th>Order Total</th>
+        <th>Order Quantity</th>
+        <th>Shipping Method</th>
+      </tr>";
 
-    	echo "
-    		<tr>
-    			<td>$orderID</td>
-    			<td style =\"text-align: center\">$orderTotal</td>
-    			<td>$quantity</td>
-    			<td>$shipping</td>
-    		</tr>
-        ";
+      // login to mySQL instance in order to query
+      require 'login.php';
+      $conn = new mysqli($hn, $un, $pw, $db);
+      if($conn->connect_error)
+        die($conn->connect_error);
+
+      // query the database for all orders made by the user
+      $query = "SELECT * FROM lab5_orders WHERE username = '$username'";
+      $result = $conn->query($query);
+      while($row = $result->fetch_array()){
+      	$orderID = $row['orderID'];
+      	$orderTotal = $row['orderTotal'];
+      	$quantity = $row['quantity'];
+      	$shipping = $row['shipping'];
+
+      	echo "
+      		<tr>
+      			<td>$orderID</td>
+      			<td style =\"text-align: center\">$orderTotal</td>
+      			<td>$quantity</td>
+      			<td>$shipping</td>
+      		</tr>
+          ";
+      }
+
+      echo "</table>";
+      echo "<p><a href=\"logout_page.php\">Log out</a></p>";
     }
   ?>
-</table>
-<p><a href="logout_page.php">Log out</a></p>
 </body>
-
 </html>
